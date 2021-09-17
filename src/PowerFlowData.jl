4e52b65cdfb7f81e1fd5d64b06ab5315309c816b
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2021-09-17 20:49:55 +0100

    Document `Generators` columns

--- src/PowerFlowData.jl
+++ src/PowerFlowData.jl
@@ -201,25 +201,106 @@ type code of two (2) or three (3) must have a generator data record entered for 
 $TYPEDFIELDS
 """
 struct Generators <: Records
+    "Bus number, or extended bus name enclosed in single quotes."
     i::Vector{Int}
+    """
+    One- or two-character uppercase non blank alphanumeric machine identifier used to distinguish among multiple machines at bus "I".
+    It is recommended that, at buses for which a single machine is present, the machine be designated as having the machine identifier ’1’.
+    ID = ’1’ by default.
+    """
     id::Vector{String}
+    "Generator active power output; entered in MW. PG = 0.0 by default."
     pg::Vector{Float64}
+    """
+    Generator reactive power output; entered in Mvar.
+    QG needs to be entered only if the case, as read in, is to be treated as a solved case.
+    QG = 0.0 by default.
+    """
     qg::Vector{Float64}
+    """
+    Maximum generator reactive power output; entered in Mvar.
+    For fixed output gen- erators (i.e., nonregulating), QT must be equal to the fixed Mvar output.
+    QT = 9999.0 by default.
+    """
     qt::Vector{Float64}
+    """
+    Minimum generator reactive power output; entered in Mvar.
+    For fixed output generators, QB must be equal to the fixed Mvar output.
+    QB = -9999.0 by default.
+    """
     qb::Vector{Float64}
+    "Regulated voltage setpoint; entered in pu. VS = 1.0 by default."
     vs::Vector{Float64}
+    """
+    Bus number, or extended bus name enclosed in single quotes,
+    of a remote type 1 or 2 bus whose voltage is to be regulated by this plant to the value specified by VS.
+    If bus IREG is other than a type 1 or 2 bus, bus "I" regulates its own voltage to the value specified by VS.
+    IREG is entered as zero if the plant is to regulate its own voltage and must be zero for a type three (swing) bus.
+    IREG = 0 by default.
+    """
     ireg::Vector{Int}
+    """
+    Total MVA base of the units represented by this machine; entered in MVA.
+    This quantity is not needed in normal power flow and equivalent construction work,
+    but is required for switching studies, fault analysis, and dynamic simulation.
+    MBASE = system base MVA by default.
+    """
     mbase::Vector{Float64}
+    """
+    Complex machine impedance, ZSORCE; entered in pu on MBASE base.
+    This data is not needed in normal power flow and equivalent construction work,
+    but is required for switching studies, fault analysis, and dynamic simulation.
+    For dynamic simulation, this impedance must be set equal to the unsaturated subtransient impedance for those generators to be modeled by subtransient level machine models,
+    and to unsaturated transient impedance for those to be modeled by classical or transient level models.
+    For short-circuit studies, the saturated subtransient or transient impedance should be used.
+    ZR = 0.0 by default.
+    """
     zr::Vector{Float64}
+    "See `zr`. ZX = 1.0 by default."
     zx::Vector{Float64}
+    """
+    Step-up transformer impedance, XTRAN; entered in pu on MBASE base.
+    XTRAN should be entered as zero if the step-up transformer is explicitly modeled as a network branch and bus "I" is the terminal bus.
+    RT+jXT = 0.0 by default.
+    """
     rt::Vector{Float64}
+    "See `rt`. RT+jXT = 0.0 by default."
     xt::Vector{Float64}
+    """
+    Step-up transformer off-nominal turns ratio; entered in pu.
+    GTAP is used only if XTRAN is nonzero.
+    GTAP = 1.0 by default.
+    """
     gtap::Vector{Float64}
+    """
+    Initial machine status of one for in-service and zero for out-of-service.
+    STAT = 1 by default.
+    """
     stat::Vector{Int}
+    """
+    Percent of the total Mvar required to hold the voltage at the bus controlled by this bus "I" that are to be contributed by the generation at bus "I";
+    RMPCT must be positive.
+    RMPCT is needed if IREG specifies a valid remote bus and there is more than one local or remote voltage controlling device
+    (plant, switched shunt, FACTS device shunt element, or VSC dc line converter) controlling the voltage at bus IREG to a setpoint.
+    RMPCT is needed also if bus "I" itself is being controlled locally or remotely by one or more other setpoint mode voltage controlling devices.
+    RMPCT = 100.0 by default.
+    """
     rmpct::Vector{Float64}
+    "Maximum generator active power output; entered in MW. PT = 9999.0 by default."
     pt::Vector{Float64}
+    "Minimum generator active power output; entered in MW. PB = -9999.0 by default."
     pb::Vector{Float64}
+    """
+    Owner number; (1 through the maximum number of owners at the current size level).
+    Each machine may have up to four owners.
+    By default, O1 is the owner to which bus "I" is assigned and O2, O3, and O4 are zero.
+    """
     oi::Vector{Int}
+    """
+    Fraction of total ownership assigned to owner Oi; each Fi must be positive.
+    The Fi values are normalized such that they sum to 1.0 before they are placed in the working case.
+    By default, each Fi is 1.0.
+    """
     fi::Vector{Float64}
 end
 
