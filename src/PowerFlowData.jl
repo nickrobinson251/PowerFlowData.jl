bac22b3a455645de6e69c4da1427d8aa3278001e
Nick Robinson <npr251@gmail.com>
2021-10-02 11:49:45 +0100

    Swap WeakRefStrings.jl -> InlineStrings.jl (#24)
    * Swap WeakRefStrings.jl -> InlineStrings.jl
    
    Since InlineString is all we used from WeakRefStrings,
    we can just use the lighter-weight InlineStrings.jl directly.
    
    * Update docs deps too

--- src/PowerFlowData.jl
+++ src/PowerFlowData.jl
@@ -4,7 +4,7 @@ using DocStringExtensions
 using Parsers: Parsers, xparse
 using Parsers: codes, eof, invalid, invaliddelimiter, newline, peekbyte
 using Tables
-using WeakRefStrings: InlineString3, InlineString15
+using InlineStrings: InlineString3, InlineString15
 
 export parse_network
 export Network
